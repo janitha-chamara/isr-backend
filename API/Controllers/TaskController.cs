c1cf858d53bac27f994586a32df16ca5b644de87
Pathum Chamara <pathum.chamarae@sra.com.au>
2022-12-01 06:57:49 +1030

    changes

--- API/Controllers/TaskController.cs
+++ API/Controllers/TaskController.cs
@@ -68,68 +68,68 @@ namespace ISRAPI.Controllers
 
         }
 
-          public BaseResponse<int> UpdateTaskTemp(IList<TaskDto> tasklist)
-        {
-            decimal? estimatetocomplite = 0;
-            decimal? currentquotedhoursUsed = 0;
-            decimal? totalforecostHours = 0;
-            decimal? CurrentTroughProject = 0;
-            decimal? forecastquoteHours = 0;
-            decimal? ah = 0;
-            decimal? qh = 0;
-            int isnullCompleteHours = 0;
-            int jobid = 0;
-            foreach (var item in tasklist)
-            {
-                jobid = item.JobId;
-                ah += item.ActualHours ?? 0;
-                qh += item.QuotedHours ?? 0;
-                estimatetocomplite += item.EstToComplHours ?? 0;
-                totalforecostHours += item.TotalForecastHours ?? 0;
+        //  public BaseResponse<int> UpdateTaskTemp(IList<TaskDto> tasklist)
+        //{
+        //    decimal? estimatetocomplite = 0;
+        //    decimal? currentquotedhoursUsed = 0;
+        //    decimal? totalforecostHours = 0;
+        //    decimal? CurrentTroughProject = 0;
+        //    decimal? forecastquoteHours = 0;
+        //    decimal? ah = 0;
+        //    decimal? qh = 0;
+        //    int isnullCompleteHours = 0;
+        //    int jobid = 0;
+        //    foreach (var item in tasklist)
+        //    {
+        //        jobid = item.JobId;
+        //        ah += item.ActualHours ?? 0;
+        //        qh += item.QuotedHours ?? 0;
+        //        estimatetocomplite += item.EstToComplHours ?? 0;
+        //        totalforecostHours += item.TotalForecastHours ?? 0;
 
 
-                //if (item.ActualHours != null && item.EstToComplHours == null)
-                //{
-                //    isnullCompleteHours += 1;
-                //}
-                //if (totalforecostHours != null || totalforecostHours != 0)
-                //{
-                //    if (estimatetocomplite == 0)
-                //    {
-                //        estimatetocomplite = null;
-                //        CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
+        //        //if (item.ActualHours != null && item.EstToComplHours == null)
+        //        //{
+        //        //    isnullCompleteHours += 1;
+        //        //}
+        //        //if (totalforecostHours != null || totalforecostHours != 0)
+        //        //{
+        //        //    if (estimatetocomplite == 0)
+        //        //    {
+        //        //        estimatetocomplite = null;
+        //        //        CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
 
-                //    }
-                //    else
-                //    {
-                //        CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
+        //        //    }
+        //        //    else
+        //        //    {
+        //        //        CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
 
-                //    }
-                //}
+        //        //    }
+        //        //}
 
-                if (item.ActualHours == null && item.EstToComplHours == null && item.EstToComplHours == null || item.ActualHours == null && item.EstToComplHours == 0)
-                {
-                    item.TotalForecastHours = item.QuotedHours;
-                    item.EstToComplHours = null;
-                }
-                //if (estimatetocomplite == 0)
-                //{
-                //    estimatetocomplite = null;
-                //}
+        //        if (item.ActualHours == null && item.EstToComplHours == null && item.EstToComplHours == null || item.ActualHours == null && item.EstToComplHours == 0)
+        //        {
+        //            item.TotalForecastHours = item.QuotedHours;
+        //            item.EstToComplHours = null;
+        //        }
+        //        //if (estimatetocomplite == 0)
+        //        //{
+        //        //    estimatetocomplite = null;
+        //        //}
 
-                currentquotedhoursUsed = (ah / qh) * 100;
-                forecastquoteHours = (totalforecostHours / qh ?? 0) * 100;
+        //        currentquotedhoursUsed = (ah / qh) * 100;
+        //        forecastquoteHours = (totalforecostHours / qh ?? 0) * 100;
 
-                var result = _taskService.UpdateTask(item.ToTaskModel());
-            }
+        //        var result = _taskService.UpdateTask(item.ToTaskModel());
+        //    }
 
-            CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
+        //    CurrentTroughProject = (totalforecostHours - estimatetocomplite) / totalforecostHours * 100;
 
-            var id = _jobService.UpdateEstimatetoComplete(currentquotedhoursUsed, forecastquoteHours, estimatetocomplite, totalforecostHours, CurrentTroughProject, jobid);
+        //    var id = _jobService.UpdateEstimatetoComplete(currentquotedhoursUsed, forecastquoteHours, estimatetocomplite, totalforecostHours, CurrentTroughProject, jobid);
 
-            return new BaseResponse<int>(1);
+        //    return new BaseResponse<int>(1);
 
-        }
+        //}
 
     }
 }
